89e0c2924f33bf01e08f58bb633255cecdb9988a
Il mastro Stefanuzzo <71498817+IlmastroStefanuzzo@users.noreply.github.com>
2021-09-15 14:09:47 +0200

    Update weather.bash

--- weather.bash
+++ weather.bash
@@ -1,3 +1,3 @@
 weather () {
-    (curl -s wttr.in/${CITY}?0 || (echo && (((`tput cols` > 43)) && cowsay -f bud-frogs 'Could not get info about current weather' || cowsay -f tux 'No weather info') | lolcat -ftS -26)) | tail -n +2
+    (curl -s wttr.in/${LOCATION}?0 || (echo && ([ $(tput cols) -gt 43 ] && cowsay -f bud-frogs 'Could not get info about current weather' || cowsay -f tux 'No weather info') | lolcat -ftS -26)) | tail -n +2
 }
